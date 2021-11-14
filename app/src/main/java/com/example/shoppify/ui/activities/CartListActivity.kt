a49e40c77e038e6e5f9eddec0fdcfd74ec29677b
Jatin Jain <jatinbjain@gmail.com>
2021-11-14 11:24:05 +0530

    Shoppify App

--- app/src/main/java/com/example/shoppify/ui/activities/CartListActivity.kt
+++ app/src/main/java/com/example/shoppify/ui/activities/CartListActivity.kt
@@ -3,9 +3,12 @@ package com.example.shoppify.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppify.R
 import com.example.shoppify.firestore.FirestoreClass
 import com.example.shoppify.models.CartItem
+import com.example.shoppify.ui.adapters.CartItemsListAdapter
 import kotlinx.android.synthetic.main.activity_cart_list.*
 import kotlinx.android.synthetic.main.activity_product_details.*
 import kotlinx.android.synthetic.main.activity_product_details.toolbar_product_details_activity
@@ -32,8 +35,44 @@ class CartListActivity : BaseActivity() {
     fun successCartItemsList(cartList: ArrayList<CartItem>) {
 
         hideProgressDialog()
-        for (i in cartList) {
-            Log.i("Cart Item Title", i.title)
+        if (cartList.size > 0) {
+
+            rv_cart_items_list.visibility = View.VISIBLE
+            ll_checkout.visibility = View.VISIBLE
+            tv_no_cart_item_found.visibility = View.GONE
+
+            rv_cart_items_list.layoutManager = LinearLayoutManager(this@CartListActivity)
+            rv_cart_items_list.setHasFixedSize(true)
+
+            val cartListAdapter = CartItemsListAdapter(this@CartListActivity, cartList)
+            rv_cart_items_list.adapter = cartListAdapter
+
+            var subTotal: Double = 0.0
+
+            for (item in cartList) {
+
+                val price = item.price.toDouble()
+                val quantity = item.cart_quantity.toInt()
+
+                subTotal += (price * quantity)
+            }
+
+            tv_sub_total.text = "Rs. $subTotal"
+            tv_shipping_charge.text = "Rs. 10.0"
+
+            if (subTotal > 0) {
+                ll_checkout.visibility = View.VISIBLE
+
+                val total = subTotal + 10
+                tv_total_amount.text = "Rs. $total"
+            } else {
+                ll_checkout.visibility = View.GONE
+            }
+
+        } else {
+            rv_cart_items_list.visibility = View.GONE
+            ll_checkout.visibility = View.GONE
+            tv_no_cart_item_found.visibility = View.VISIBLE
         }
     }
     private fun getCartItemsList() {
