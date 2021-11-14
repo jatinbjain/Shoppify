88942dceb9faeb67b0099a7253c49bef2718c7a4
Jatin Jain <jatinbjain@gmail.com>
2021-11-14 10:54:23 +0530

    Shoppify App

--- app/src/main/java/com/example/shoppify/ui/activities/CartListActivity.kt
+++ app/src/main/java/com/example/shoppify/ui/activities/CartListActivity.kt
@@ -2,11 +2,49 @@ package com.example.shoppify.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.shoppify.R
+import com.example.shoppify.firestore.FirestoreClass
+import com.example.shoppify.models.CartItem
+import kotlinx.android.synthetic.main.activity_cart_list.*
+import kotlinx.android.synthetic.main.activity_product_details.*
+import kotlinx.android.synthetic.main.activity_product_details.toolbar_product_details_activity
 
-class CartListActivity : AppCompatActivity() {
+class CartListActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart_list)
+        setupActionBar()
+    }
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_cart_list_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        }
+
+        toolbar_cart_list_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    fun successCartItemsList(cartList: ArrayList<CartItem>) {
+
+        hideProgressDialog()
+        for (i in cartList) {
+            Log.i("Cart Item Title", i.title)
+        }
+    }
+    private fun getCartItemsList() {
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        FirestoreClass().getCartList(this@CartListActivity)
+    }
+    override fun onResume() {
+        super.onResume()
+
+        getCartItemsList()
     }
 }
