21ae57c285d913a2f9edff7bde3b7d3753c0868a
Jatin Jain <jatinbjain@gmail.com>
2021-11-14 15:47:22 +0530

    Shoppify App

---
+++ app/src/main/java/com/example/shoppify/models/Address.kt
@@ -0,0 +1,4 @@
+package com.example.shoppify.models
+
+class Address {
+}
--- app/src/main/java/com/example/shoppify/ui/activities/AddEditAddressActivity.kt
+++ app/src/main/java/com/example/shoppify/ui/activities/AddEditAddressActivity.kt
@@ -2,11 +2,12 @@ package com.example.shoppify.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import com.example.shoppify.R
 import kotlinx.android.synthetic.main.activity_add_edit_address.*
 import kotlinx.android.synthetic.main.activity_address_list.*
 
-class AddEditAddressActivity : AppCompatActivity() {
+class AddEditAddressActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_edit_address)
@@ -24,4 +25,46 @@ class AddEditAddressActivity : AppCompatActivity() {
 
         toolbar_add_edit_address_activity.setNavigationOnClickListener { onBackPressed() }
     }
+
+    private fun validateData(): Boolean {
+        return when {
+
+            TextUtils.isEmpty(et_full_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_please_enter_full_name),
+                    true
+                )
+                false
+            }
+
+            TextUtils.isEmpty(et_phone_number.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_please_enter_phone_number),
+                    true
+                )
+                false
+            }
+
+            TextUtils.isEmpty(et_address.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_please_enter_address), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_zip_code.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_please_enter_zip_code), true)
+                false
+            }
+
+            rb_other.isChecked && TextUtils.isEmpty(
+                et_zip_code.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_please_enter_zip_code), true)
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
+
 }
